e4a7ce46e05c15da3ebf3032be57af5bf4a88405
Maheshkumar-patil <maheshpatil8336@gmail.com>
2023-03-02 16:12:05 +0530

    add functionality

--- JS/shop.js
+++ JS/shop.js
@@ -16,4 +16,50 @@ function myFunction() {
       }
     }
   }
-   
+
+
+  // ==============  Delete Products From List ===================== //
+
+  let deleteButton = document.getElementById('delete-button');
+deleteButton.addEventListener('click', deleteSelectedProducts);
+
+function deleteSelectedProducts() {
+  // Get all the checkbox input elements
+  let checkboxes = document.querySelectorAll('#product-list input[type="checkbox"]');
+
+  // Loop through the checkboxes to find the checked ones
+  for (let i = 0; i < checkboxes.length; i++) {
+    let checkbox = checkboxes[i];
+    if (checkbox.checked) {
+      // Get the parent div element and delete it
+      let productDiv = checkbox.closest('.product-card');
+      productDiv.remove();
+    }
+  }
+}
+
+   // ==============  Filter Products From List ===================== //
+
+   let categoryFilter = document.querySelector('#category-filter');
+categoryFilter.addEventListener('change', filterProducts);
+
+function filterProducts() {
+  // Get the selected option element and its value
+  let categoryFilter = document.querySelector('#category-filter');
+  let categoryValue = categoryFilter.value;
+
+  // Get all the product div elements
+  let productDivs = document.querySelectorAll('#product-list .product-card');
+
+  // Loop through the product divs to show or hide them based on the filter value
+  for (let i = 0; i < productDivs.length; i++) {
+    let productDiv = productDivs[i];
+    let productCategory = productDiv.getAttribute('data-category');
+
+    if (categoryValue === '' || productCategory === categoryValue) {
+      productDiv.style.display = 'block';
+    } else {
+      productDiv.style.display = 'none';
+    }
+  }
+}
