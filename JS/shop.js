4ec5f553e2483c3f59c9c455c50069000662725b
Maheshkumar-patil <maheshpatil8336@gmail.com>
2023-03-03 13:00:43 +0530

    functionality

--- JS/shop.js
+++ JS/shop.js
@@ -63,3 +63,56 @@ function filterProducts() {
     }
   }
 }
+
+
+// ======================== Search bar Open ======================= //
+
+function openSearch() {
+  document.getElementById("myOverlay").style.display = "block";
+}
+
+function closeSearch() {
+  document.getElementById("myOverlay").style.display = "none";
+}
+
+
+const searchInput = document.getElementById('search-input');
+searchInput.addEventListener('keyup', search);
+
+function search() {
+  // Get the user's search query
+  const query = document.getElementById('search-input').value.toLowerCase();
+
+  // Loop through each product div and show/hide based on search query
+  const products = document.querySelectorAll('.product-card');
+  products.forEach((product) => {
+    const name = product.getAttribute('data-name').toLowerCase();
+    if (name.includes(query)) {
+      product.style.display = 'block';
+    } else {
+      product.style.display = 'none';
+    }
+  });
+}
+
+
+// ============== Select All Checkbox ==================//
+
+function selectAll() {
+  const checkboxItems = document.querySelectorAll('input[type="checkbox"]');
+  let allSelected = true;
+
+  checkboxItems.forEach(item => {
+    if (!item.checked) {
+      allSelected = false;
+      item.checked = true;
+    }
+  });
+
+  if (allSelected) {
+    checkboxItems.forEach(item => {
+      item.checked = false;
+    });
+  }
+}
+
