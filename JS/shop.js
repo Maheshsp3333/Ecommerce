a1ec8e33c47c47d3212322c48cd868b3b796bbb7
Maheshkumar-patil <maheshpatil8336@gmail.com>
2023-03-11 16:18:22 +0530

    JAVASCRIPT

--- JS/shop.js
+++ JS/shop.js
@@ -120,7 +120,9 @@ editButton.addEventListener("click", editSelectedProducts);
 
 function editSelectedProducts() {
   // Get all the checkbox input elements
-  let checkboxes = document.querySelectorAll('#product-list input[type="checkbox"]');
+  let checkboxes = document.querySelectorAll(
+    '#product-list input[type="checkbox"]'
+  );
 
   // Loop through the checkboxes to find the checked ones
   for (let i = 0; i < checkboxes.length; i++) {
@@ -130,8 +132,152 @@ function editSelectedProducts() {
       let productPrice = prompt("Edit Product Price");
       if (productName != null && productPrice != null) {
         checkbox.closest(".product-card").children[2].innerHTML = productName;
-        checkbox.closest(".product-card").children[3].innerHTML = "$"+productPrice;
+        checkbox.closest(".product-card").children[3].innerHTML =
+          "$" + productPrice;
       }
     }
   }
 }
+
+// =============== Add Product To The Cart ==========================//
+
+let cartIcon = document.querySelector("#cart-icon");
+let cart = document.querySelector(".cart");
+let closeCart = document.querySelector("#close-cart");
+
+// open cart
+cartIcon.onclick = () => {
+  cart.classList.add("active");
+};
+
+// close cart
+closeCart.onclick = () => {
+  cart.classList.remove("active");
+};
+
+// cart working
+
+if (document.readyState == "loading") {
+  document.addEventListener("DOMContentLoaded", ready);
+} else {
+  ready();
+}
+
+// making function
+
+function ready() {
+  // remove item cart
+  var removeCartButtons = document.getElementsByClassName("cart-remove");
+
+  for (var i = 0; i < removeCartButtons.length; i++) {
+    var button = removeCartButtons[i];
+    button.addEventListener("click", removeCartItem);
+  }
+
+  // Quantity changes
+  var quantityInputs = document.getElementsByClassName("cart-quantity");
+  for (var i = 0; i < quantityInputs.length; i++) {
+    var input = quantityInputs[i];
+    input.addEventListener("change", quantityChanged);
+  }
+}
+
+// remove from cart
+function removeCartItem(event) {
+  var buttonClicked = event.target;
+  buttonClicked.parentElement.remove();
+  updatetotal();
+}
+// quantity change
+
+function quantityChanged(event) {
+  var input = event.target;
+  if (isNaN(input.value) || input.value <= 0) {
+    input.value = 1;
+  }
+  updatetotal();
+}
+
+//  =============== Add To Cart Items ======================= //
+
+function addSelectedProducts() {
+  // Get all the checkbox input elements
+  let checkboxes = document.querySelectorAll(
+    '#product-list input[type="checkbox"]'
+  );
+  // Loop through the checkboxes to find the checked ones
+  for (let i = 0; i < checkboxes.length; i++) {
+    let checkbox = checkboxes[i];
+    if (checkbox.checked) {
+      let pname = checkbox.closest(".product-card").children[2].innerHTML;
+      let pricename = checkbox.closest(".product-card").children[3].innerHTML;
+      let productimg = checkbox.closest(".product-card").children[1].src;
+
+      // var cart = document.getElementsByClassName(".cart-content")|| [];
+      // var isProductInCart = false;
+      // for (var i = 0; i < cart.length; i++) {
+      //   if (
+      //     cart[i].name === pname &&
+      //     cart[i].price === pricename &&
+      //     cart[i].image === productimg
+      //   ) {
+      //     isProductInCart = true;
+      //     break;
+      //   }
+      // }
+      // if (!isProductInCart) {
+      //   cart.push(cartShopBox);
+      // }
+
+      var cartShopBox = document.createElement("div");
+
+      cartShopBox.innerHTML = `
+            <div class="cart-box">
+            <img src="${productimg}" alt="" class="cart-img">
+            <div class="detail-box">
+              <div class="cart-product-title">${pname}</div>
+              <div class="cart-price">${pricename}</div>
+              <input type="number" value="1" class="cart-quantity">
+            </div>
+            
+            <i class="fa fa-trash cart-remove" aria-hidden="true"></i>
+          </div> `;
+
+      cartShopBox
+        .getElementsByClassName("cart-remove")[0]
+        .addEventListener("click", removeCartItem);
+      cartShopBox
+        .getElementsByClassName("cart-quantity")[0]
+        .addEventListener("change", quantityChanged);
+
+      // add cart item to cart
+      var cartItems = document.querySelector(".cart-content");
+      cartItems.appendChild(cartShopBox);
+    }
+  }
+  updatetotal();
+}
+
+// update total
+
+function updatetotal() {
+  var cartContent = document.getElementsByClassName("cart-content")[0];
+  var cartBoxes = cartContent.getElementsByClassName("cart-box");
+  var total = 0;
+  for (var i = 0; i < cartBoxes.length; i++) {
+    var cartBox = cartBoxes[i];
+    var priceElement = cartBox.getElementsByClassName("cart-price")[0];
+    var quantityElement = cartBox.getElementsByClassName("cart-quantity")[0];
+    var price = parseFloat(priceElement.innerText.replace("$", ""));
+    var quantity = quantityElement.value;
+    total = total + price * quantity;
+
+    // if price contain same
+
+    total = Math.round(total * 100) / 100;
+
+    document.getElementsByClassName("total-price")[0].innerText = "$" + total;
+  }
+}
+
+// =============== Add Product End =================================//
